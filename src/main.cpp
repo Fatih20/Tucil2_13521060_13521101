477e99c7358d0ef809db7157d0013ccf191312cb
Fatih20 <fnri39@protonmail.com>
2023-02-25 15:00:13 +0700

    Minor tidying up of main.

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,33 @@ int main()
     int pointCount;
     cout << "Enter the number of points : ";
     cin >> pointCount;
-    srand(time(NULL));
+    cout << endl;
     DotCollection dc(dimension, pointCount);
 
-    cout << "TOTAL POINTS: " << dc.length() << endl;
-    cout << "DIMENSION: " << dimension << endl;
     using namespace chrono;
     auto start = high_resolution_clock::now();
     ClosestPairData closest = dc.getClosestPair();
     auto end = high_resolution_clock::now();
 
     auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "DIVIDE AND CONQUER\n";
+    cout << "Devide et Impera\n";
     cout << (double)duration.count() / 1e6 << " seconds" << endl;
-    cout << "TOTAL DISTANCE CALCULATION : " << Dot::getTotalEuclidean() << endl;
+    cout << "Number of Distance Calculation : " << Dot::getTotalEuclidean() << endl;
 
     closest.getFirstDot().print();
     closest.getSecondDot().print();
     cout << closest.getDistance() << endl;
+
     Dot::resetTotalEuclidean();
+
     start = high_resolution_clock::now();
     ClosestPairData closestBrute = dc.getClosestPairBruteForce();
     end = high_resolution_clock::now();
-
+    cout << endl;
     duration = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "BRUTE FORCE\n";
+    cout << "Smol brain O(n^2)\n";
     cout << (double)duration.count() / 1e6 << " seconds" << endl;
-    cout << "TOTAL DISTANCE CALCULATION : " << Dot::getTotalEuclidean() << endl;
+    cout << "Number of Distance Calculation : " << Dot::getTotalEuclidean() << endl;
 
     closest.getFirstDot().print();
     closest.getSecondDot().print();
